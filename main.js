1856579a3119064674003f6396283fbfa2bd57a5
Imgyeong Lee <leeim@oregonstate.edu>
2022-05-20 14:11:38 -0700

    Test doctor

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ app.use('/people_certs', require('./people_certs.js'));
 app.use('/people', require('./people.js'));
 app.use('/pharmacy-insert', require('./pharmacy-insert.js'));
 app.use('/pharmacy-insert2', require('./pharmacy-insert2.js'));
-app.use('/doctor', require('./doctor.js'));
+app.use('/doctor', require('./doctor1.js'));
 app.use('/medication', require('./medication.js'));
 app.use('/patient', require('./patient.js'));
 app.use('/pharmacy', require('./pharmacy.js'));
