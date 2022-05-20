d720a83aa55bb5d8d329857634d546c918a74f44
Imgyeong Lee <leeim@oregonstate.edu>
2022-05-19 22:54:28 -0700

    TEST

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_huangjef',
-  password        : 'Yunhai8dog',
-  database        : 'cs340_huangjef'
+  user            : 'cs340_leeim',
+  password        : 'wp3wkdldirl',
+  database        : 'cs340_leeim'
 });
 module.exports.pool = pool;
