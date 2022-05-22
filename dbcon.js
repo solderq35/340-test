d5b80def7af0831c1ff81329f334c446c57486c2
solderq35 <solderq35@gmail.com>
2022-05-21 23:15:01 -0700

    remove clone folder, fix gitignore

--- dbcon.js
+++ dbcon.js
@@ -1,9 +1,5 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_huangjef',
-  password        : 'Yunhai8dog',
-  database        : 'cs340_huangjef'
+//insert MySQL credentials here
 });
 module.exports.pool = pool;
