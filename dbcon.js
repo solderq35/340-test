de9a6b16e5728ab9f9b8a992eec8b57396c1dde8
solderq35 <solderq35@gmail.com>
2022-05-21 23:24:12 -0700

    cached

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_huangjef',
-  password        : 'Yunhai8dog',
-  database        : 'cs340_huangjef'
-});
-module.exports.pool = pool;
