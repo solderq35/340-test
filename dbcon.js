e72ed987f54e2047b121ab2f27ae5b455cc33cad
solderq35 <solderq35@gmail.com>
2022-05-22 11:13:22 -0700

    gitignorechange

---
+++ dbcon.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : '127.0.0.1',
+  user            : 'root',
+  password        : 'Yunhai8doggum',
+  database        : 'yourDB'
+});
+module.exports.pool = pool;
