4071df7361af4fd88c51efb82bfb131ac4ceff1d
solderq35 <solderq35@gmail.com>
2022-05-21 23:16:15 -0700

    gitignore fix 2

--- dbcon.js
+++ dbcon.js
@@ -1,5 +1,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-//insert MySQL credentials here
+  connectionLimit : 10,
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs340_huangjef',
+  password        : 'Yunhai8dog',
+  database        : 'cs340_huangjef'
 });
 module.exports.pool = pool;
