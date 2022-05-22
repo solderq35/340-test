52638b2641650ee08e3003d431b08f25b71e65f7
solderq35 <solderq35@gmail.com>
2022-05-21 23:34:36 -0700

    cache

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'us-cdbr-east-05.cleardb.net',
-  user            : 'ba86a3bbb7c52c',
-  password        : '2b3ecac8',
-  database        : 'heroku_14a6fb8a75d52d2'
-});
-module.exports.pool = pool;
