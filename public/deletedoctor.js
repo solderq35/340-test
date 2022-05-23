df6acb11f1255761016be2c9ce841f0990362658
solderq35 <solderq35@gmail.com>
2022-05-23 10:27:00 -0700

    delete working on mediphar

--- public/deletedoctor.js
+++ public/deletedoctor.js
@@ -22,3 +22,18 @@ function deletePeopleCert(pid, cid){
       }
   })
 };
+
+function deletemediphar(medication_id, pharmacy_id){
+  $.ajax({
+      url: '/mediphar/medication_id/' + medication_id + '/pharmacy_id/' + pharmacy_id,
+      type: 'DELETE',
+      success: function(result){
+          if(result.responseText != undefined){
+            alert(result.responseText)
+          }
+          else {
+            window.location.reload(true)
+          } 
+      }
+  })
+};
