8b6e1b2582c6dc482cdb2129db8806ed268c98ed
solderq35 <82061589+solderq35@users.noreply.github.com>
2022-06-02 18:18:50 -0700

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'us-cdbr-east-05.cleardb.net',
-  user            : 'ba86a3bbb7c52c',
-  password        : 'f0a9b9239bcbb42',
-  database        : 'heroku_14a6fb8a75d52d2'
-});
-module.exports.pool = pool;
